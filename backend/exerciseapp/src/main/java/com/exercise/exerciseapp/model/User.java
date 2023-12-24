c47e54b3d319d688e9ae7aaab434b663bff6c3b3
sebassil12 <anddysilva15@gmail.com>
2023-12-24 13:11:28 -0500

    login and register fully

--- backend/exerciseapp/src/main/java/com/exercise/exerciseapp/model/User.java
+++ backend/exerciseapp/src/main/java/com/exercise/exerciseapp/model/User.java
@@ -3,8 +3,10 @@ package com.exercise.exerciseapp.model;
 
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
@@ -83,28 +85,25 @@ public class User implements UserDetails{
     }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+        return List.of(new SimpleGrantedAuthority(role.name()) );
+            
     }
+    
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
+        return true;
     }
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
+        return true;
     }
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
+        return true;
     }
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
+        return true;
     }
    
     //Class Builder
--- backend/exerciseapp/src/main/resources/db/migration/V1__adduser.sql
+++ backend/exerciseapp/src/main/resources/db/migration/V1__adduser.sql
@@ -2,8 +2,9 @@ CREATE TABLE user_app(
     user_id SERIAL,
     username VARCHAR(45) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    password VARCHAR(45) NOT NULL,
+    password VARCHAR(245) NOT NULL,
     is_active BOOLEAN,
+    role VARCHAR(45),
     PRIMARY KEY (user_id)
 
 )
